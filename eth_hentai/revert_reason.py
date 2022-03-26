3d57bf93167d0e1f54dda924e457a44d920f3b20
Mikko Ohtamaa <mikko@opensourcehacker.com>
2022-03-26 11:26:42 +0100

    Better logging error message

--- eth_hentai/revert_reason.py
+++ eth_hentai/revert_reason.py
@@ -118,11 +118,16 @@ def fetch_transaction_revert_reason(
         # Ethereum Tester
         return e.args[0]
 
+    # TODO:
+    # Not sure why this happens.
+    # When checking on bscchain:
+    # This transaction has been included and will be reflected in a short while.
+
     receipt = web3.eth.get_transaction_receipt(tx_hash)
     if receipt.status != 0:
         logger.error("Queried revert reason for a transaction, but receipt tells it did not fail. tx_hash:%s, receipt: %s", tx_hash.hex(), receipt)
 
     current_block_number = web3.eth.block_number
     # TODO: Convert to logger record
-    logger.error(f"Transaction succeeded, when it should have failed. Hash: {tx_hash.hex()}, tx block num: {tx.blockNumber}, current block number: {current_block_number}, transaction result {result.hex()}. Maybe the chain tip is unstable?")
+    logger.error(f"Transaction succeeded, when we tried to fetch its revert reason. Hash: {tx_hash.hex()}, tx block num: {tx.blockNumber}, current block number: {current_block_number}, transaction result {result.hex()}. Maybe the chain tip is unstable?")
     return unknown_error_message
