de5372d7912e1949e57f08241edd667a923ca673
Mikko Ohtamaa <mikko@opensourcehacker.com>
2022-04-03 00:09:08 +0200

    RTD bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,6 +57,7 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+
 autodoc_class_signature = "separated"
 
 autodoc_typehints = "description"
