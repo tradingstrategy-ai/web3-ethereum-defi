60f994badf29487eb5cd52359cb92fbf46e5e5dd
Hieu Nguyen <hieu@tradingstrategy.ai>
2022-05-30 01:38:29 +0300

    Construct tick csv

--- eth_defi/uniswap_v3/liquidity.py
+++ eth_defi/uniswap_v3/liquidity.py
@@ -1,5 +1,7 @@
+"""Uniswap v3 liquidity events"""
 import csv
-from typing import Callable, Iterable, TypedDict
+from functools import reduce
+from typing import Iterable, TypedDict
 
 import pandas as pd
 
@@ -92,12 +94,13 @@ def create_tick_delta_csv(
     mints_csv: str,
     burns_csv: str,
     output_folder: str = "/tmp",
-):
+) -> str:
     """Create intermediate tick delta csv based on mint and burn events
 
     :param mints_csv: Path to mint events CSV
     :param burns_csv: Path to burn events CSV
     :param output_folder: Folder to contain output CSV files, default is /tmp folder
+    :return: output CSV path
     """
     mints_df = pd.read_csv(mints_csv)
     burns_df = pd.read_csv(burns_csv)
@@ -112,7 +115,8 @@ def create_tick_delta_csv(
         keep="first",
     )
 
-    with open(f"{output_folder}/uniswapv3-tickdeltas.csv", "w") as fh:
+    file_path = f"{output_folder}/uniswapv3-tickdeltas.csv"
+    with open(file_path, "w") as fh:
         writer = csv.DictWriter(fh, fieldnames=TickDelta.__annotations__.keys())
         writer.writeheader()
 
@@ -123,3 +127,43 @@ def create_tick_delta_csv(
         for _, event in burns_df.iterrows():
             for tick_delta in handle_burn_event(event):
                 writer.writerow(tick_delta)
+
+    return file_path
+
+
+def create_tick_csv(
+    tick_delta_csv: str,
+    output_folder: str = "/tmp",
+) -> str:
+    """Create tick csv based on tick delta
+
+    :param tick_delta_csv: Path to tick delta CSV
+    :param output_folder: Folder to contain output CSV files, default is /tmp folder
+    :return: output CSV path
+    """
+    deltas_df = pd.read_csv(tick_delta_csv)
+
+    # we don't need to use block number and timestamp here
+    deltas_df = deltas_df[["pool_contract_address", "tick_id", "liquidity_gross_delta", "liquidity_net_delta"]]
+
+    def sum_int(series: pd.Series) -> int:
+        """Cast series to int then sum
+
+        Since liquidity data is loaded from csv, it has type object and the data (uint128)
+        is too big to fit to any pandas datatype
+        """
+        return reduce(lambda x, y: int(x) + int(y), series)
+
+    ticks_df = (
+        deltas_df.groupby(["pool_contract_address", "tick_id"])
+        .agg(
+            {
+                "liquidity_gross_delta": sum_int,
+                "liquidity_net_delta": sum_int,
+            }
+        )
+        .reset_index()
+    )
+
+    file_path = f"{output_folder}/uniswapv3-ticks.csv"
+    return ticks_df.to_csv(file_path)
