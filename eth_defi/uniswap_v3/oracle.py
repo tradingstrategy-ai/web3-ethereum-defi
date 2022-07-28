3cc9250eb34e480fe106108f001f3c96220a3168
Hieu Nguyen <hieu@tradingstrategy.ai>
2022-07-28 13:32:49 +0300

    Update price oracle concurrently

--- eth_defi/uniswap_v3/oracle.py
+++ eth_defi/uniswap_v3/oracle.py
@@ -3,11 +3,14 @@ import datetime
 from dataclasses import dataclass
 from decimal import Decimal
 
+from requests.adapters import HTTPAdapter
 from web3 import Web3
 
 from eth_defi.abi import get_contract
 from eth_defi.event_reader.logresult import LogContext
-from eth_defi.event_reader.reader import Filter, read_events
+from eth_defi.event_reader.reader import Filter, read_events, read_events_concurrent
+from eth_defi.event_reader.web3factory import TunedWeb3Factory
+from eth_defi.event_reader.web3worker import create_thread_pool_executor
 from eth_defi.price_oracle.oracle import PriceEntry, PriceOracle, PriceSource
 from eth_defi.uniswap_v3.events import decode_swap
 from eth_defi.uniswap_v3.pool import PoolDetails, fetch_pool_details
@@ -51,6 +54,76 @@ def convert_swap_event_to_price_entry(log: dict) -> PriceEntry:
         block_hash=log["blockHash"],
         tx_hash=swap_info["tx_hash"],
     )
+
+
+def update_price_oracle_concurrent(
+    oracle: PriceOracle,
+    json_rpc_url: str,
+    pool_contract_address: str,
+    start_block: int,
+    end_block: int,
+    reverse_token_order: bool = False,
+    max_workers: int = 16,
+):
+    """Feed price oracle data for a given block range using using a thread pool
+
+    Example:
+
+    .. code-block: python
+
+        TODO
+
+    :param oracle:
+        Price oracle to update
+
+    :param web3:
+        Web3 connection we use to fetch event data from JSON-RPC node
+
+    :param pool_contract_address:
+        Pool contract address
+
+    :param start_block:
+        First block to include data for
+
+    :param end_block:
+        Last block to include data for (inclusive)
+
+    :param reverse_token_order:
+        If pair token0 is the quote token to calculate the price.
+    """
+    http_adapter = HTTPAdapter(pool_connections=max_workers, pool_maxsize=max_workers)
+    web3_factory = TunedWeb3Factory(json_rpc_url, http_adapter)
+    web3 = web3_factory(None)
+    pool_details = fetch_pool_details(web3, pool_contract_address)
+    log_context = UniswapV3PriceOracleContext(pool_details, reverse_token_order)
+    executor = create_thread_pool_executor(web3_factory, log_context, max_workers=max_workers)
+
+    Pool = get_contract(web3, "uniswap_v3/UniswapV3Pool.json")
+
+    signatures = Pool.events.Swap.build_filter().topics
+    assert len(signatures) == 1
+
+    event_filter = Filter(
+        contract_address=pool_contract_address,
+        bloom=None,
+        topics={
+            signatures[0]: Pool.events.Swap,
+        },
+    )
+
+    # Feed oracle with event data from JSON-RPC node
+    for log_result in read_events_concurrent(
+        executor,
+        start_block,
+        end_block,
+        events=[Pool.events.Swap],
+        notify=None,
+        chunk_size=10,
+        filter=event_filter,
+        context=log_context,
+    ):
+        entry = convert_swap_event_to_price_entry(log_result)
+        oracle.add_price_entry_reorg_safe(entry)
 
 
 def update_price_oracle_single_thread(
@@ -103,6 +176,7 @@ def update_price_oracle_single_thread(
     )
 
     pool_details = fetch_pool_details(web3, pool_contract_address)
+    log_context = UniswapV3PriceOracleContext(pool_details, reverse_token_order)
 
     # Feed oracle with event data from JSON-RPC node
     for log_result in read_events(
@@ -113,7 +187,7 @@ def update_price_oracle_single_thread(
         notify=None,
         chunk_size=100,
         filter=event_filter,
-        context=UniswapV3PriceOracleContext(pool_details, reverse_token_order),
+        context=log_context,
     ):
         entry = convert_swap_event_to_price_entry(log_result)
         oracle.add_price_entry(entry)
