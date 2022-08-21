775f410dc8a41848d731ec4ea53398839dc2599d
Mikko Ohtamaa <mikko@opensourcehacker.com>
2022-08-21 20:12:19 +0200

    Ganache issues

--- tests/test_token_tax.py
+++ tests/test_token_tax.py
@@ -111,7 +111,7 @@ def uniswap(web3: Web3, pancakeswap_factory_v2: HexAddress, pancake_router: HexA
 
 
 # Because Ganache is such a crap and keeps randomly failing
-@flaky.flaky
+@flaky.flaky(max_runs=5)
 def test_token_tax(uniswap: UniswapV2Deployment, large_busd_holder: HexAddress, seller: HexAddress,
                    elephant: HexAddress, busd: HexAddress):
 
