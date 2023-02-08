b12ba7ea2c29b2fc1f5bba3ebb2d60981455df88
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2023-02-08 18:33:45 +0200

    improve tests

--- tests/test_price_conversion.py
+++ tests/test_price_conversion.py
@@ -18,17 +18,33 @@ def method2(raw_price: float, in_token_decimals: int, out_token_decimals: int, r
         return raw_price / 10 ** (out_token_decimals - in_token_decimals)
 
 
-def test_methods():
-    # path = ["0xF2E246BB76DF876Cef8b38ae84130F4F55De395b", 3000, "0xB9816fC57977D5A786E654c7CF76767be63b966e"]
-    tick = -201931
-    raw_price = tick_to_price(tick)
-    price1 = method1(raw_price, 6, 18, True)
-    price2 = method2(raw_price, 6, 18, True)
-    # assert price1 == price2
 
-    # path = ["0xB9816fC57977D5A786E654c7CF76767be63b966e", 3000, "0xF2E246BB76DF876Cef8b38ae84130F4F55De395b"]
-    tick = -201937
+def get_prices(tick: int, in_token_decimals: int, out_token_decimals: int, reverse: bool = False):
     raw_price = tick_to_price(tick)
-    price1 = method1(raw_price, 18, 6, False)
-    price2 = method2(raw_price, 18, 6, False)
-    # assert price1 == price2
+    price1 = method1(raw_price, in_token_decimals, out_token_decimals, reverse)
+    price2 = method2(raw_price, in_token_decimals, out_token_decimals, reverse)
+
+    return price1, price2
+
+def test_methods():
+    """Uses real values derived from analyse_trade_receipt usage in tradeexecutor"""
+    # path = ["0xF2E246BB76DF876Cef8b38ae84130F4F55De395b", 3000, "0xB9816fC57977D5A786E654c7CF76767be63b966e"]
+    price1, price2 = get_prices(-201931, 6, 18, True)
+    
+    print("")
+    print(price1)
+    print(price2)
+    
+    # path = ["0xB9816fC57977D5A786E654c7CF76767be63b966e", 3000, "0xF2E246BB76DF876Cef8b38ae84130F4F55De395b"]
+    price1, price2 = get_prices(-201937, 18, 6, False)
+    
+    print(price1)
+    print(price2)
+    
+    price1, price2 = get_prices(-201937, 18, 6, True)
+    
+    print(price1)
+    print(price2)
+
+
+
