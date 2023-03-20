77099672f74597df41fc5e3998aff25e3a595ccb
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-03-20 19:48:34 +0100

    More Enzyme vault functionality coverage

--- eth_defi/enzyme/vault.py
+++ eth_defi/enzyme/vault.py
@@ -137,9 +137,12 @@ class Vault:
         Call the Solidity function that does this on the smart contract side.
 
         See `ComptrollerLib.sol`.
+<<<<<<< HEAD
 
         :return:
             TODO - no idea
+=======
+>>>>>>> 7296378 (More Enzyme vault functionality coverage)
         """
         return self.comptroller.functions.calcGav().call()
 
@@ -149,9 +152,12 @@ class Vault:
         Call the Solidity function that does this on the smart contract side.
 
         See `ComptrollerLib.sol`.
+<<<<<<< HEAD
 
         :return:
             TODO - no idea
+=======
+>>>>>>> 7296378 (More Enzyme vault functionality coverage)
         """
         return self.comptroller.functions.calcGrossShareValue().call()
 
