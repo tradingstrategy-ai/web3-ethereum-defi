e6b9666328674a99ed67d4b3a4687221b905e133
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-04-02 13:09:11 +0200

    Fix Anvil Github start issues

--- eth_defi/anvil.py
+++ eth_defi/anvil.py
@@ -366,6 +366,9 @@ def launch_anvil(
         assert block_time > 0, f"Got bad block time {block_time}"
         args["block_time"] = block_time
 
+
+    current_block = chain_id = None
+
     while attempts_left > 0:
         process, final_cmd = _launch(
             cmd,
