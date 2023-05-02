d7f2cb238d97c0488fd8b91c815de5d055f59dd2
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2023-05-02 11:56:01 +0200

    add helper functions

--- eth_defi/uniswap_v3/price.py
+++ eth_defi/uniswap_v3/price.py
@@ -1,4 +1,7 @@
 """Uniswap v3 price calculations."""
+
+from decimal import Decimal
+
 from eth_typing import HexAddress
 
 from eth_defi.uniswap_v3.deployment import UniswapV3Deployment
@@ -81,3 +84,226 @@ class UniswapV3PriceHelper:
         assert len(fees) == len(path) - 1
         assert slippage >= 0
         assert type(amount) == int, "Incorrect type provided for amount. Require int"
+
+
+def estimate_buy_quantity(
+    uniswap: UniswapV3Deployment,
+    base_token_address: HexAddress,
+    quote_token_address: HexAddress,
+    quantity: int,
+    target_pair_fee: int,
+    *,
+    slippage: float = 0,
+    intermediate_token_address: HexAddress | None,
+    intermediate_pair_fee: int | None,
+) -> int:
+    """Estimate how many tokens we are going to receive when doing a buy.
+
+    Good for price impact calculations.
+
+    Calls the on-chain contract to get the current liquidity and estimates the
+    the price based on it.
+
+    Example:
+
+    .. code-block:: python
+
+        # Estimate how much ETH we will receive for 500 USDC.
+        # In this case the pool ETH price is $1700 so this should be below ~1/4 of ETH
+        amount_eth = estimate_buy_quantity(
+            uniswap_v3,
+            weth.address,
+            usdc.address,
+            500*10**18,
+            500,
+        )
+        assert amount_eth / 1e18 == pytest.approx(0.28488156127668085)
+
+    :param quantity: How much of the quote token we have to use
+    :param uniswap: Uniswap v3 deployment
+    :param base_token_address: Address of the base token of the trading pair
+    :param quote_token_address: Address of the quote token of the trading pair
+    :param target_pair_fee: Trading fee express in raw format for the target pair
+    :param slippage: Slippage express in bps
+    :return: Expected base token to receive
+    """
+    price_helper = UniswapV3PriceHelper(uniswap)
+    
+    if intermediate_token_address:
+        path = [quote_token_address, intermediate_token_address, base_token_address]
+        fees = [intermediate_pair_fee, target_pair_fee]
+    else:
+        path = [quote_token_address, base_token_address]
+        fees = [target_pair_fee]
+
+    return price_helper.get_amount_out(quantity, path, fees, slippage=slippage)
+
+
+def estimate_buy_price(
+    uniswap: UniswapV3Deployment,
+    base_token_address: HexAddress,
+    quote_token_address: HexAddress,
+    quantity: int,
+    target_pair_fee: int,
+    *,
+    slippage: float = 0,
+    intermediate_token_address: HexAddress | None,
+    intermediate_pair_fee: int | None,
+) -> int:
+    """Estimate how much we are going to need to pay when doing buy.
+
+    Calls the on-chain contract to get the current liquidity and estimates the
+    the price based on it.
+
+    Example:
+
+    .. code-block:: python
+
+        # Estimate how much ETH we will receive for 500 USDC.
+        # In this case the pool ETH price is $1700 so this should be below ~1/4 of ETH
+        amount_eth = estimate_buy_price(
+            uniswap_v3,
+            weth.address,
+            usdc.address,
+            1*10**18,
+            500,
+        )
+        assert amount_eth / 1e18 == pytest.approx(0.28488156127668085)
+
+    :param uniswap: Uniswap v2 deployment
+    :param base_token_address: Base token address of the trading pair
+    :param quote_token_address: Quote token addressof the trading pair
+    :param quantity: How much of the base token we want to buy
+    :param target_pair_fee: Trading fee express in raw format for the target pair
+    :param slippage: Slippage express in bps
+    :return: Expected base token to receive
+    """
+    price_helper = UniswapV3PriceHelper(uniswap)
+    if intermediate_token_address:
+        path = [quote_token_address, intermediate_token_address, base_token_address]
+        fees = [intermediate_pair_fee, target_pair_fee]
+    else:
+        path = [quote_token_address, base_token_address]
+        fees = [target_pair_fee]
+
+    return price_helper.get_amount_in(quantity, path, fees, slippage=slippage)
+
+
+def estimate_sell_price(
+    uniswap: UniswapV3Deployment,
+    base_token_address: HexAddress,
+    quote_token_address: HexAddress,
+    quantity: int,
+    target_pair_fee: int,
+    *,
+    slippage: float = 0,
+    intermediate_token_address: HexAddress | None,
+    intermediate_pair_fee: int | None,
+) -> int:
+    """Estimate how much we are going to get paid when doing a sell.
+
+    Calls the on-chain contract to get the current liquidity and estimates the
+    the price based on it.
+
+    .. note ::
+
+        The price of an asset depends on how much you are selling it. More you sell,
+        more there will be price impact.
+
+    To get a price of an asset, ask for quantity 1 of it:
+
+    :param quantity: How much of the base token we want to sell
+    :param uniswap: Uniswap v3 deployment
+    :param base_token_address: Base token address of the trading pair
+    :param quote_token_address: Quote token address of the trading pair
+    :param target_pair_fee: Trading fee of the target pair expressed in raw
+    :param slippage: Slippage express in bps
+    :return: Expected quote token amount to receive
+    """
+    price_helper = UniswapV3PriceHelper(uniswap)
+    if intermediate_token_address:
+        path = [base_token_address, intermediate_token_address, quote_token_address]
+        fees = [intermediate_pair_fee, target_pair_fee]
+    else:
+        path = [base_token_address, quote_token_address]
+        fees = [target_pair_fee]
+
+    return price_helper.get_amount_in(quantity, path, fees, slippage=slippage)
+
+
+def estimate_buy_received_amount_raw(
+    uniswap: UniswapV3Deployment,
+    base_token_address: HexAddress,
+    quote_token_address: HexAddress,
+    quantity_raw: Decimal,
+    target_pair_fee: int,
+    *,
+    slippage: float = 0,
+    intermediate_token_address: HexAddress | None,
+    intermediate_pair_fee: int | None,
+) -> int:
+    
+    fee_helper = UniswapV3PriceHelper(uniswap)
+
+    if intermediate_token_address:
+        path = [quote_token_address, intermediate_token_address, base_token_address]
+        fees = [intermediate_pair_fee, target_pair_fee]
+    else:
+        path = [quote_token_address, base_token_address]
+        fees = [target_pair_fee]
+
+    # We will receive equal number of amounts as there are items in the path
+    return fee_helper.get_amount_out(quantity_raw, path, fees, slippage=slippage)
+
+
+def estimate_sell_received_amount_raw(
+    uniswap: UniswapV3Deployment,
+    base_token_address: HexAddress,
+    quote_token_address: HexAddress,
+    quantity_raw: Decimal,
+    target_pair_fee: int,
+    *,
+    slippage: float = 0,
+    intermediate_token_address: HexAddress | None,
+    intermediate_pair_fee: int | None,
+) -> int:
+    """Estimate how much we receive for a certain cash amount.
+
+    Example:
+
+    .. code-block:: python
+
+        # Estimate the price of buying 1650 USDC worth of ETH
+        eth_received = estimate_buy_received_amount_raw(
+            uniswap_v3,
+            weth.address,
+            usdc.address,
+            1650 * 10**18,
+            500,
+        )
+
+        assert eth_received / (10**18) == pytest.approx(0.9667409780905836)
+
+        # Calculate price of ETH as $ for our purchase
+        price = (1650*10**18) / eth_received
+        assert price == pytest.approx(Decimal(1706.7653460381143))
+
+    :param quantity: How much of the base token we want to buy
+    :param uniswap: Uniswap v3 deployment
+    :param base_token_address: Base token address of the trading pair
+    :param quote_token_address: Quote token address of the trading pair
+    :param target_pair_fee: Trading fee of the target pair in raw format
+    :param slippage: Slippage express in bps
+    :return: Expected quote token amount to receive
+    :raise TokenDetailError: If we have an issue with ERC-20 contracts
+    """
+    price_helper = UniswapV3PriceHelper(uniswap)
+
+    if intermediate_token_address:
+        path = [base_token_address, intermediate_token_address, quote_token_address]
+        fees = [intermediate_pair_fee, target_pair_fee]
+    else:
+        path = [base_token_address, quote_token_address]
+        fees = [target_pair_fee]
+
+    return price_helper.get_amount_out(quantity_raw, path, fees, slippage=slippage)
