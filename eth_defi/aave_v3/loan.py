36ce912749645c86906da78a6d2a2e9297eca0ca
Hieu Nguyen <hieu@tradingstrategy.ai>
2023-05-03 13:12:57 +0300

    Merge approve token to supply process

--- eth_defi/aave_v3/loan.py
+++ eth_defi/aave_v3/loan.py
@@ -1,73 +1,18 @@
-"""Aave v3 deposit."""
+"""Aave v3 loan"""
 from typing import Callable
 
-from web3 import Web3
+from web3.contract import Contract
 
-from eth_defi.abi import get_abi_by_filename
 from eth_defi.hotwallet import HotWallet
+from eth_defi.trace import assert_transaction_success_with_explanation
 
 
-def approve_token(web3: Web3, token_address: str, spender: str, amount: int) -> Callable:
-    """
-    Approve a specified amount of token to be spent.
-
-    Example:
-
-    .. code-block:: python
-
-        # build transaction to approve ERC-20 to be spent.
-        approve_func = approve_token(
-            web3=web3,
-            token_address=token_address,
-            spender=spender,
-            amount=amount
-        )
-
-        tx = approve_func.build_transaction(
-            {
-                "from": hot_wallet_address,
-                "chainId": web3.eth.chain_id
-            }
-        )
-        tx = fill_nonce(web3, tx)
-
-        # sign and broadcast
-        signed = hot_wallet_account.sign_transaction(tx)
-        tx_hash = web3.eth.send_raw_transaction(signed.rawTransaction)
-        receipt = web3.eth.get_transaction_receipt(tx_hash)
-        assert receipt.status == 1, "USDC transfer reverted"
-
-    :param web3:
-        Web3 Instance
-    :param token_address:
-        The address of the Aave v3 reserve token that you want to deposit.
-    :param spender:
-        The address of the spender to be approved.
-    :param amount:
-        The amount of token to be approved.
-    :return:
-        Callable approve transaction.
-    """
-
-    # Load token contract ABI
-    token_abi = get_abi_by_filename("ERC20MockDecimals.json")["abi"]
-
-    # Get token contract instance
-    token_contract = web3.eth.contract(address=token_address, abi=token_abi)
-
-    # Approve the token contract to spend the specified amount of token
-    transaction = token_contract.functions.approve(spender, amount)
-
-    # Return the transaction
-    return transaction
-
-
-def deposit_in_aave(
-    web3: Web3,
-    hot_wallet: HotWallet,
-    aave_deposit_address: str,
-    token_address: str,
+def supply(
+    aave_v3_deployment,
+    *,
+    token: Contract,
     amount: int,
+    hot_wallet: HotWallet,
 ) -> Callable:
     """
     Opens a loan position in Aave v3 by depositing any Aave v3 reserve token and receiving aToken back.
@@ -76,50 +21,47 @@ def deposit_in_aave(
 
     .. code-block:: python
 
-        # build transaction to deposit a reserve token
-        deposit_func = deposit_in_aave(
-            web3=web3,
-            hot_wallet=hot_wallet_account,
-            aave_deposit_address=AAVE_DEPOSIT_ADDRESS,
-            token_address=USDC_ADDRESS,
-            amount=amount
+        # build transaction to supply USDC to Aave v3
+        supply_func = supply(
+            aave_v3_deployment=aave_v3_deployment,
+            hot_wallet=hot_wallet,
+            token=usdc.contract,
+            amount=amount,
         )
 
-        tx = deposit_func.build_transaction(
+        tx = supply_func.build_transaction(
             {
-                "from": hot_wallet_address,
+                "from": hot_wallet.address,
                 "chainId": web3.eth.chain_id
             }
         )
-        tx = fill_nonce(web3, tx)
 
         # sign and broadcast
-        signed = hot_wallet_account.sign_transaction(tx)
+        signed = hot_wallet_account.sign_transaction_with_new_nonce(tx)
         tx_hash = web3.eth.send_raw_transaction(signed.rawTransaction)
-        receipt = web3.eth.get_transaction_receipt(tx_hash)
-        assert receipt.status == 1, "Aave v3 deposit reverted"
+        assert_transaction_success_with_explanation(web3, tx_hash)
 
-    :param web3:
-        Web3 Instance.
+    :param aave_v3_deployment:
+        Instance of :py:class:`eth_defi.aave_v3.deployment.AaveV3Deployment`.
+    :param token:
+        Aave v3 reserve token you want to supply.
     :param hot_wallet:
         Instance of :py:class:`eth_defi.hotwallet.Hotwallet`.
-    :param aave_deposit_address:
-        The address of the Aave v3 contract where the deposit will be made.
-    :param token_address:
-        The address of the Aave v3 reserve token that you want to deposit.
     :param amount:
-        The amount of token to deposit.
+        The amount of token to supply.
     :return:
-        Callable deposit transaction.
+        Callable supply transaction.
     """
-    # Load Aave v3 contract ABI
-    aave_v3_abi = get_abi_by_filename("aave_v3/Pool.json")["abi"]
+    web3 = aave_v3_deployment.web3
+    pool = aave_v3_deployment.pool
 
-    # Get the Aave v3 contract instance
-    aave_v3_contract = web3.eth.contract(address=aave_deposit_address, abi=aave_v3_abi)
+    # TODO: maybe validate if Aave v3 actually supports this asset reserve
 
-    # Approve the Aave v3 contract to supply the specified amount of token
-    transaction = aave_v3_contract.functions.supply(token_address, amount, hot_wallet.address, 0)
+    # approve to supply
+    tx = token.functions.approve(pool.address, amount).build_transaction({"from": hot_wallet.address})
+    signed = hot_wallet.sign_transaction_with_new_nonce(tx)
+    tx_hash = web3.eth.send_raw_transaction(signed.rawTransaction)
+    assert_transaction_success_with_explanation(web3, tx_hash)
 
-    # Return the transaction
-    return transaction
+    # return the supply function
+    return pool.functions.supply(token.address, amount, hot_wallet.address, 0)
---
+++ tests/aave_v3/test_aave_v3_loan.py
@@ -0,0 +1,131 @@
+"""
+    export JSON_RPC_POLYGON=https://polygon-rpc.com/
+    pytest -k test_aave_v3_loan.py
+"""
+
+import logging
+import os
+import shutil
+
+import pytest
+from eth_account import Account
+from eth_account.signers.local import LocalAccount
+from eth_typing import HexAddress, HexStr
+from web3 import HTTPProvider, Web3
+
+from eth_defi.aave_v3.constants import AAVE_V3_NETWORKS, AaveToken
+from eth_defi.aave_v3.deployment import AaveV3Deployment
+from eth_defi.aave_v3.loan import supply
+from eth_defi.abi import get_deployed_contract
+from eth_defi.anvil import fork_network_anvil
+from eth_defi.chain import install_chain_middleware
+from eth_defi.gas import node_default_gas_price_strategy
+from eth_defi.hotwallet import HotWallet
+from eth_defi.token import fetch_erc20_details
+from eth_defi.trace import assert_transaction_success_with_explanation
+
+pytestmark = pytest.mark.skipif(
+    (os.environ.get("JSON_RPC_POLYGON") is None) or (shutil.which("anvil") is None),
+    reason="Set JSON_RPC_POLYGON env in order to run these tests",
+)
+
+
+@pytest.fixture()
+def anvil_polygon_chain_fork(request, large_usdc_holder) -> str:
+    """Create a testable fork of live Polygon chain.
+    :return: JSON-RPC URL for Web3
+    """
+    launch = fork_network_anvil(os.environ["JSON_RPC_POLYGON"], unlocked_addresses=[large_usdc_holder])
+    try:
+        yield launch.json_rpc_url
+    finally:
+        # Wind down Anvil process after the test is complete
+        launch.close(log_level=logging.ERROR)
+
+
+@pytest.fixture()
+def web3(anvil_polygon_chain_fork: str):
+    """Set up a local unit testing blockchain."""
+    # https://web3py.readthedocs.io/en/stable/examples.html#contract-unit-tests-in-python
+    web3 = Web3(HTTPProvider(anvil_polygon_chain_fork))
+    # Anvil needs POA middlware if parent chain needs POA middleware
+    install_chain_middleware(web3)
+    web3.eth.set_gas_price_strategy(node_default_gas_price_strategy)
+    return web3
+
+
+@pytest.fixture()
+def large_usdc_holder() -> HexAddress:
+    """A random account picked from Polygon that holds a lot of USDC.
+    `To find large USDC holder accounts, use polygoscan <https://polygonscan.com/token/0x2791bca1f2de4661ed88a30c99a7a9449aa84174#balances>`_.
+    """
+    # Binance Hot Wallet 6
+    return HexAddress(HexStr("0x06959153B974D0D5fDfd87D561db6d8d4FA0bb0B"))
+
+
+@pytest.fixture()
+def hot_wallet(web3) -> HotWallet:
+    """User account."""
+    hw = HotWallet(Account.create())
+    hw.sync_nonce(web3)
+    return hw
+
+
+@pytest.fixture()
+def usdc(web3):
+    """Get USDC on Polygon."""
+    return fetch_erc20_details(web3, "0x2791bca1f2de4661ed88a30c99a7a9449aa84174")
+
+
+@pytest.fixture()
+def aave_v3_polygon_deployment(web3):
+    return AaveV3Deployment(
+        web3=web3,
+        pool=get_deployed_contract(web3, "aave_v3/Pool.json", address=AAVE_V3_NETWORKS["polygon"].pool_address),
+    )
+
+
+@pytest.fixture()
+def aave_v3_usdc_reserve() -> AaveToken:
+    return AAVE_V3_NETWORKS["polygon"].token_contracts["USDC"]
+
+
+def test_aave_v3_supply(
+    web3: Web3,
+    aave_v3_polygon_deployment,
+    usdc,
+    large_usdc_holder: HexAddress,
+    hot_wallet: LocalAccount,
+    aave_v3_usdc_reserve,
+):
+    """Test that the deposit in Aave v3 is correctly registered and the corresponding aToken is received."""
+    supply_amount = 100 * 10**6
+
+    # give hot wallet some native token and USDC
+    web3.eth.send_transaction(
+        {
+            "from": large_usdc_holder,
+            "to": hot_wallet.address,
+            "value": 100 * 10**18,
+        }
+    )
+    usdc.contract.functions.transfer(
+        hot_wallet.address,
+        supply_amount * 2,
+    ).transact({"from": large_usdc_holder})
+
+    # supply USDC to Aave
+    tx = supply(
+        aave_v3_deployment=aave_v3_polygon_deployment,
+        hot_wallet=hot_wallet,
+        token=usdc.contract,
+        amount=supply_amount,
+    ).build_transaction({"from": hot_wallet.address})
+    signed = hot_wallet.sign_transaction_with_new_nonce(tx)
+    tx_hash = web3.eth.send_raw_transaction(signed.rawTransaction)
+
+    assert_transaction_success_with_explanation(web3, tx_hash)
+
+    # verify aUSDC token amount in hot wallet
+    ausdc_details = fetch_erc20_details(web3, aave_v3_usdc_reserve.deposit_address)
+    assert ausdc_details.contract.functions.balanceOf(hot_wallet.address).call() == supply_amount
