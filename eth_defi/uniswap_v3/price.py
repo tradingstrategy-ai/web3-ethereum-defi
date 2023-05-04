4c7531f64a83cb13a5bbd8ea535d2a6597733d64
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2023-05-04 11:36:39 +0200

    V3 helpers (#115)

--- eth_defi/uniswap_v3/price.py
+++ eth_defi/uniswap_v3/price.py
@@ -1,4 +1,7 @@
 """Uniswap v3 price calculations."""
+
+from decimal import Decimal
+
 from eth_typing import HexAddress
 
 from eth_defi.uniswap_v3.deployment import UniswapV3Deployment
@@ -81,3 +84,81 @@ class UniswapV3PriceHelper:
         assert len(fees) == len(path) - 1
         assert slippage >= 0
         assert type(amount) == int, "Incorrect type provided for amount. Require int"
+
+
+def estimate_buy_received_amount(
+    uniswap: UniswapV3Deployment,
+    base_token_address: HexAddress,
+    quote_token_address: HexAddress,
+    quantity: Decimal | int,
+    target_pair_fee: int,
+    *,
+    slippage: float = 0,
+    intermediate_token_address: HexAddress | None = None,
+    intermediate_pair_fee: int | None = None,
+) -> int:
+    
+    fee_helper = UniswapV3PriceHelper(uniswap)
+
+    if intermediate_token_address:
+        path = [quote_token_address, intermediate_token_address, base_token_address]
+        fees = [intermediate_pair_fee, target_pair_fee]
+    else:
+        path = [quote_token_address, base_token_address]
+        fees = [target_pair_fee]
+
+    # We will receive equal number of amounts as there are items in the path
+    return fee_helper.get_amount_out(quantity, path, fees, slippage=slippage)
+
+
+def estimate_sell_received_amount(
+    uniswap: UniswapV3Deployment,
+    base_token_address: HexAddress,
+    quote_token_address: HexAddress,
+    quantity: Decimal | int,
+    target_pair_fee: int,
+    *,
+    slippage: float = 0,
+    intermediate_token_address: HexAddress | None = None,
+    intermediate_pair_fee: int | None = None,
+) -> int:
+    """Estimate how much we receive for a certain cash amount.
+
+    Example:
+
+    .. code-block:: python
+
+        # Estimate the price of buying 1650 USDC worth of ETH
+        eth_received = estimate_buy_received_amount_raw(
+            uniswap_v3,
+            weth.address,
+            usdc.address,
+            1650 * 10**18,
+            500,
+        )
+
+        assert eth_received / (10**18) == pytest.approx(0.9667409780905836)
+
+        # Calculate price of ETH as $ for our purchase
+        price = (1650*10**18) / eth_received
+        assert price == pytest.approx(Decimal(1706.7653460381143))
+
+    :param quantity: How much of the base token we want to buy
+    :param uniswap: Uniswap v3 deployment
+    :param base_token_address: Base token address of the trading pair
+    :param quote_token_address: Quote token address of the trading pair
+    :param target_pair_fee: Trading fee of the target pair in raw format
+    :param slippage: Slippage express in bps
+    :return: Expected quote token amount to receive
+    :raise TokenDetailError: If we have an issue with ERC-20 contracts
+    """
+    price_helper = UniswapV3PriceHelper(uniswap)
+
+    if intermediate_token_address:
+        path = [base_token_address, intermediate_token_address, quote_token_address]
+        fees = [intermediate_pair_fee, target_pair_fee]
+    else:
+        path = [base_token_address, quote_token_address]
+        fees = [target_pair_fee]
+
+    return price_helper.get_amount_out(quantity, path, fees, slippage=slippage)
