2e58f2456a0146200137e69202b08484cb68a13c
Mikko Ohtamaa <mikko@opensourcehacker.com>
2024-01-11 23:48:49 +0200

    More guards

--- contracts/guard/src/GuardV0.sol
+++ contracts/guard/src/GuardV0.sol
@@ -35,14 +35,32 @@ contract GuardV0 is IGuard, Ownable {
     // Allowed ERC-20 tokens we may receive or send in a trade
     mapping(address token => bool allowed) public allowedAssets;
 
-    // Allowed trade executors
+    // Allowed trade executor hot wallets
     mapping(address sender => bool allowed) public allowedSenders;
+
+    // Allowed token receivers post trade
+    mapping(address receiver => bool allowed) public allowedReceivers;
+
+    // Allowed owners
+    mapping(address destination => bool allowed) public allowedWithdrawDestinations;
+
+    // Allowed routers
+    mapping(address destination => bool allowed) public allowedApprovalDestinations;
 
     event CallSiteApproved(address target, bytes4 selector, string notes);
     event CallSiteRemoved(address target, bytes4 selector, string notes);
 
     event SenderApproved(address sender, string notes);
     event SenderRemoved(address sender, string notes);
+
+    event ReceiverApproved(address sender, string notes);
+    event ReceiverRemoved(address sender, string notes);
+
+    event WithdrawDestinationApproved(address sender, string notes);
+    event WithdrawDestinationRemoved(address sender, string notes);
+
+    event ApprovalDestinationApproved(address sender, string notes);
+    event ApprovalDestinationRemoved(address sender, string notes);
 
     event AssetApproved(address sender, string notes);
     event AssetRemoved(address sender, string notes);
@@ -62,7 +80,7 @@ contract GuardV0 is IGuard, Ownable {
         return owner();
     }
 
-    function approveCallSite(address target, bytes4 selector, string calldata notes) public onlyOwner {
+    function allowCallSite(address target, bytes4 selector, string calldata notes) public onlyOwner {
         allowedCallSites[target][selector] = true;
         emit CallSiteApproved(target, selector, notes);
     }
@@ -80,6 +98,36 @@ contract GuardV0 is IGuard, Ownable {
     function removeSender(address sender, string calldata notes) public onlyOwner {
         delete allowedSenders[sender];
         emit SenderRemoved(sender, notes);
+    }
+
+    function allowReceiver(address receiver, string calldata notes) public onlyOwner {
+        allowedReceivers[receiver] = true;
+        emit ReceiverApproved(receiver, notes);
+    }
+
+    function removeReceiver(address receiver, string calldata notes) public onlyOwner {
+        delete allowedReceivers[receiver];
+        emit ReceiverRemoved(receiver, notes);
+    }
+
+    function allowWithdrawDestination(address destination, string calldata notes) public onlyOwner {
+        allowedWithdrawDestinations[destination] = true;
+        emit WithdrawDestinationApproved(destination, notes);
+    }
+
+    function removeWithdrawDestination(address destination, string calldata notes) public onlyOwner {
+        delete allowedWithdrawDestinations[destination];
+        emit WithdrawDestinationRemoved(destination, notes);
+    }
+
+    function allowApprovalDestination(address destination, string calldata notes) public onlyOwner {
+        allowedApprovalDestinations[destination] = true;
+        emit ApprovalDestinationApproved(destination, notes);
+    }
+
+    function removeApprovalDestination(address destination, string calldata notes) public onlyOwner {
+        delete allowedApprovalDestinations[destination];
+        emit ApprovalDestinationRemoved(destination, notes);
     }
 
     function allowAsset(address sender, string calldata notes) public onlyOwner {
@@ -101,9 +149,17 @@ contract GuardV0 is IGuard, Ownable {
         return allowedSenders[sender] == true;
     }
 
-    // For now we assume sender = generic adapter = receiver
-    function isAllowedReceiver(address receiver) public view returns (bool) {
-        return isAllowedSender(receiver);
+    // Assume any tokens are send back to the vaule
+    function isAllowedReceiver(address sender) public view returns (bool) {
+        return isAllowedSender(sender);
+    }
+
+    function isAllowedWithdrawDestination(address receiver) public view returns (bool) {
+        return allowedWithdrawDestinations[receiver] == true;
+    }
+
+    function isAllowedApprovalDestination(address receiver) public view returns (bool) {
+        return allowedApprovalDestinations[receiver] == true;
     }
 
     function isAllowedAsset(address token) public view returns (bool) {
@@ -118,6 +174,16 @@ contract GuardV0 is IGuard, Ownable {
         require(isAllowedReceiver(to), "Receiver address does not match");
         require(isAllowedAsset(tokenIn), "Token in not allowed");
         require(isAllowedAsset(tokenOut), "Token out not allowed");
+    }
+
+    function validate_transfer(bytes memory callData) public view {
+        (address to, ) = abi.decode(callData, (address, uint));
+        require(isAllowedWithdrawDestination(to), "Receiver address does not match");
+    }
+
+    function validate_approve(bytes memory callData) public view {
+        (address to, ) = abi.decode(callData, (address, uint));
+        require(isAllowedApprovalDestination(to), "Approve address does not match");
     }
 
     function validateCall(
@@ -136,12 +202,27 @@ contract GuardV0 is IGuard, Ownable {
         // Assume sender is trade-executor hot wallet
 
         bytes4 selector = bytes4(callDataWithSelector[:4]);
+        bytes calldata callData = callDataWithSelector[4:];
         require(!isGoodCallTarget(target, selector), "Call site not allowed");
 
         if(selector == getSelector("swapTokensForExactTokens(uint,uint,address[],address,uint)")) {
-            validate_swapTokensForExactTokens(callDataWithSelector[4:]);
+            validate_swapTokensForExactTokens(callData);
+        } else if(selector == getSelector("transfer(address,uint)")) {
+            validate_transfer(callData);
+        } else if(selector == getSelector("approve(address,uint)")) {
+            validate_approve(callData);
         } else {
             revert("Unknown function selector");
         }
+    }
+
+    function whitelistToken(address token, string calldata notes) external {
+        allowCallSite(token, getSelector("transfer(address,uint)"), notes);
+        allowCallSite(token, getSelector("approve(address,uint)"), notes);
+    }
+
+    function whitelistUniswapV2Router(address router, string calldata notes) external {
+        allowCallSite(router, getSelector("swapTokensForExactTokens(uint,uint,address[],address,uint)"), notes);
+        allowApprovalDestination(router, notes);
     }
 }
---
+++ contracts/guard/src/SimpleVaultV0.sol
@@ -0,0 +1,57 @@
+/**
+ * A very simple vault implementation.
+ *
+ */
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/access/Ownable.sol";
+
+import "./GuardV0.sol";
+
+
+/**
+ * Simple vault allowing delegating of a trading activites to a hot wallet.@author
+ *
+ * - Guard is used to check asset manager can only perform approved operations.
+ * - No shares, single owner
+ * - No accounting
+ */
+contract SimpleVaultV0 is Ownable {
+
+    address public assetManager;
+
+    address public withdrawAddress;
+
+    GuardV0 public guard;
+
+    constructor() Ownable() {
+        guard = new GuardV0();
+        // The owner of the guard is the vault creator, not the vault itself
+        guard.transferOwnership(msg.sender);
+        guard.allowWithdrawDestination(msg.sender, "Initial owner can withdraw");
+        guard.allowReceiver(address(this), "Vault can receive tokens from a trade");
+    }
+
+    function getWithdrawAddress() public view returns (address) {
+        return owner();
+    }
+
+    function updateAssetManager(address _assetManager, string calldata notes) external onlyOwner {
+        if(assetManager != address(0)) {
+            guard.removeSender(assetManager, notes);
+        }
+        assetManager = _assetManager;
+        guard.allowSender(_assetManager, notes);
+    }
+
+    function performCall(address target, bytes calldata callData) external {
+
+        // Check that the asset manager can perform this function
+        guard.validateCall(msg.sender, target, callData);
+
+        (bool success, bytes memory returnData) = target.call(callData);
+        require(success, string(returnData));
+    }
+
+}
