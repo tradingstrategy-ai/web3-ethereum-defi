a3bf92eda068e440d0f66c58243b67e919601e29
Mikko Ohtamaa <mikko@opensourcehacker.com>
2024-01-11 22:45:33 +0200

    It compiles

--- contracts/guard/src/GuardV0.sol
+++ contracts/guard/src/GuardV0.sol
@@ -8,7 +8,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/access/Ownable.sol";
 
 interface IGuard {
-    function validateCall(address target, bytes callData) public;
+    function validateCall(address sender, address target, bytes memory callDataWithSelector) external;
 }
 
 interface IUniswapV2Router02 {
@@ -38,19 +38,21 @@ contract GuardV0 is IGuard, Ownable {
     // Allowed trade executors
     mapping(address sender => bool allowed) public allowedSenders;
 
-    // Allowed ERC20.approve()
-    mapping(address target => bool allowed) public uniswapV2Routers;
-
-    mapping(address target => bool allowed) public uniswapV2Routers;
-
     event CallSiteApproved(address target, bytes4 selector, string notes);
     event CallSiteRemoved(address target, bytes4 selector, string notes);
 
     event SenderApproved(address sender, string notes);
     event SenderRemoved(address sender, string notes);
 
-    constructor() Ownable {
-        governance = owner;
+    event AssetApproved(address sender, string notes);
+    event AssetRemoved(address sender, string notes);
+
+    constructor() Ownable() {
+    }
+
+    function getSelector(string memory _func) internal pure returns (bytes4) {
+        // https://solidity-by-example.org/function-selector/
+        return bytes4(keccak256(bytes(_func)));
     }
 
     /**
@@ -60,32 +62,38 @@ contract GuardV0 is IGuard, Ownable {
         return owner();
     }
 
-    function approveCallSite(address target, bytes4 selector, string notes) onlyOwner {
+    function approveCallSite(address target, bytes4 selector, string calldata notes) public onlyOwner {
         allowedCallSites[target][selector] = true;
         emit CallSiteApproved(target, selector, notes);
     }
 
-    function removeCallSite(address target, bytes4 selector) onlyOwner {
+    function removeCallSite(address target, bytes4 selector, string calldata notes) public onlyOwner {
         delete allowedCallSites[target][selector];
-        emit CallSiteApproved(target, selector);
+        emit CallSiteRemoved(target, selector, notes);
     }
 
-    function allowSender(address sender, string notes) onlyOwner {
-        allowedSender[sender] = true;
+    function allowSender(address sender, string calldata notes) public onlyOwner {
+        allowedSenders[sender] = true;
         emit SenderApproved(sender, notes);
     }
 
-    function removeSender(address sender, string notes) onlyOwner {
-        delete allowedSender[sender];
+    function removeSender(address sender, string calldata notes) public onlyOwner {
+        delete allowedSenders[sender];
         emit SenderRemoved(sender, notes);
     }
 
-    function removeCallSite(address target, bytes4 selector, string notes) onlyOwner {
-        delete allowedCallSites[target][selector];
-        emit CallSiteApproved(target, selector, notes);
+    function allowAsset(address sender, string calldata notes) public onlyOwner {
+        allowedAssets[sender] = true;
+        emit AssetApproved(sender, notes);
     }
+
+    function removeAsset(address sender, string calldata notes) public onlyOwner {
+        delete allowedAssets[sender];
+        emit AssetRemoved(sender, notes);
+    }
+
     // Basic check if any target contract is whitelisted
-    function canCall(address target, bytes4 selector) public view returns (bool) {
+    function isGoodCallTarget(address target, bytes4 selector) public view returns (bool) {
         return allowedCallSites[target][selector];
     }
 
@@ -93,35 +101,45 @@ contract GuardV0 is IGuard, Ownable {
         return allowedSenders[sender] == true;
     }
 
+    // For now we assume sender = generic adapter = receiver
+    function isAllowedReceiver(address receiver) public view returns (bool) {
+        return isAllowedSender(receiver);
+    }
+
     function isAllowedAsset(address token) public view returns (bool) {
-        return allowedAssets[sender] == true;
+        return allowedAssets[token] == true;
     }
 
     // Validate Uniswap v2 trade
-    function validate_swapTokensForExactTokens(bytes callData) public {
-        (uint amountOut, uint amountInMax, address[] path, address to, uint deadline) = abi.decode(callData, uint, uint, address[], address, uint);
+    function validate_swapTokensForExactTokens(bytes memory callData) public view {
+        (, , address[] memory path, address to, ) = abi.decode(callData, (uint, uint, address[], address, uint));
         address tokenIn = path[0];
-        address tokenOut = path[-1];
-        require(isAllowedToken(tokenIn), "Token in not allowed");
-        require(isAllowedToken(tokenOut), "Token out not allowed");
+        address tokenOut = path[path.length - 1];
+        require(isAllowedReceiver(to), "Receiver address does not match");
+        require(isAllowedAsset(tokenIn), "Token in not allowed");
+        require(isAllowedAsset(tokenOut), "Token out not allowed");
     }
 
-    function validateCall(address sender, address target, bytes callData) {
+    function validateCall(
+        address sender,
+        address target,
+        bytes calldata callDataWithSelector
+    ) external view {
 
         if(sender == getGovernanceAddress()) {
             // Governance can manually recover any issue
             return;
         }
 
-        requre(!isAllowedCaller(sender), "Sender not allowed");
+        require(!isAllowedSender(sender), "Sender not allowed");
 
         // Assume sender is trade-executor hot wallet
 
-        bytes4 selector = bytes4(callData[:4]);
-        require(!canCall(target, selector), "Call site not allowed");
+        bytes4 selector = bytes4(callDataWithSelector[:4]);
+        require(!isGoodCallTarget(target, selector), "Call site not allowed");
 
-        if(selector == abi.encodeCall(IUniswapV2Router02.swapTokensForExactTokens)) {
-            validate_swapTokensForExactTokens(callData[4:]);
+        if(selector == getSelector("swapTokensForExactTokens(uint,uint,address[],address,uint)")) {
+            validate_swapTokensForExactTokens(callDataWithSelector[4:]);
         } else {
             revert("Unknown function selector");
         }
