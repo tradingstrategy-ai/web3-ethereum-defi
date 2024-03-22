1b9ab61d0304e8c01ffd17f7de53181392427043
Mikko Ohtamaa <mikko@opensourcehacker.com>
2024-03-22 16:05:51 +0100

    Whitelist Quickswap by default on Polygon

--- eth_defi/uniswap_v2/constants.py
+++ eth_defi/uniswap_v2/constants.py
@@ -6,7 +6,7 @@ UNISWAP_V2_DEPLOYMENTS = {
 }
 
 QUICKSWAP_DEPLOYMENTS = {
-    "ethereum": {
+    "polygon": {
         "factory": "0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32",
         "router": "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff",
     },
