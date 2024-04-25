5f6fcdf6212cb1a744be35d184c6d0332fe1133c
Hieu Nguyen <hieu@tradingstrategy.ai>
2024-04-25 12:15:23 +0300

    Rename IPCFlaky exception

--- eth_defi/event_reader/fast_json_rpc.py
+++ eth_defi/event_reader/fast_json_rpc.py
@@ -5,20 +5,17 @@ Monkey-patches JSON decoder to use ujson.
 
 import logging
 from json import JSONDecodeError
-
 from typing import cast
 
 import ujson
-
 from web3 import Web3
 from web3.providers import JSONBaseProvider
 from web3.types import RPCResponse
 
-
 logger = logging.getLogger(__name__)
 
 
-class IPCFlaky(JSONDecodeError):
+class PartialHttpResponseException(JSONDecodeError):
     """IPCProvider expects JSONDecodeErrors, not value errors."""
 
 
@@ -30,7 +27,7 @@ def _fast_decode_rpc_response(raw_response: bytes) -> RPCResponse:
         # We received partial JSON-RPC response over IPC.
         # Signal the underlying stack to keep reading
         # See IPCProvider.make_request()
-        raise IPCFlaky("Partial IPC?", "", 0) from e
+        raise PartialHttpResponseException("Suspected partial HTTP response", "", 0) from e
     return cast(RPCResponse, decoded)
 
 
