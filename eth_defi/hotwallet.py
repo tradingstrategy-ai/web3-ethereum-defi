22cca6a73755d6cd0c782afa1b8f5d9e3e75b6aa
Mikko Ohtamaa <mikko@redinnovation.com>
2024-12-30 12:50:06 +0100

    Add code example

--- eth_defi/hotwallet.py
+++ eth_defi/hotwallet.py
@@ -365,15 +365,44 @@ class HotWallet:
 
         .. note ::
 
-            Before calling this method, you need to set ``gas`` and
-            ``chainId`` fields of ``tx``,
-            which is the max gas limit (not price).
+            Before calling this method, you need to set ``gas`` and ``chainId`` fields of ``tx``.
 
         Example:
 
         .. code-block:: python
 
+            private_key = os.environ["PRIVATE_KEY"]
+            # Configure direct-to-sequencer broadcast,
+            # use public Base node for reads
+            rpc_configuration_line = "mev+https://mainnet-sequencer.base.org https://mainnet.base.org"
+            web3 = create_multi_provider_web3(rpc_configuration_line)
 
+            assert web3.eth.chain_id == 8453  # Base
+
+            hot_wallet = HotWallet.from_private_key(private_key)
+            hot_wallet.sync_nonce(web3)
+
+            # As a test transaction, send very small amount of ETH
+            tx_data = {
+                "chainId": web3.eth.chain_id,
+                "from": hot_wallet.address,
+                "to": "0x7612A94AafF7a552C373e3124654C1539a4486A8",  # Random addy
+                "value": Web3.to_wei(Decimal("0.000001"), "ether"),
+                "gas": 50_000,
+            }
+
+            hot_wallet.fill_in_gas_price(web3, tx_data)
+            signed_tx = hot_wallet.sign_transaction_with_new_nonce(tx_data)
+
+            # Blocks until included in a block
+            print("Broadcasting", signed_tx.hash.hex())
+            receipts = wait_and_broadcast_multiple_nodes_mev_blocker(
+                web3.provider,
+                txs=[signed_tx],
+            )
+
+            receipt = receipts[signed_tx.hash]
+            print(f"Transaction broadcasted:\n{pformat(dict(receipt.items()))}")
 
         :param tx:
             Transaction data as a dictionary.
