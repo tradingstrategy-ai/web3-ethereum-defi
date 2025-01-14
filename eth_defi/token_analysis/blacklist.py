33d0ab7c908c23ca23276ef3b9b8b56abffe4a4c
Mikko Ohtamaa <mikko@redinnovation.com>
2025-01-14 13:50:05 +0100

    Fix test_lagoon_post_valuation() live value test

--- eth_defi/token_analysis/blacklist.py
+++ eth_defi/token_analysis/blacklist.py
@@ -5,6 +5,11 @@ from eth_typing import HexAddress
 # Valuation model failed GenericValuation failed for position <Open position #185 <Pair BOB-WETH spot_market_hold at 0x511088edf4c6fd71b48ca4fe4467d39a3c9e32e3 (1.0000% fee) on exchange uniswap-v3> $12.905446608819052>
 # Position debug data: {'balance_updates': {63: {'asset': {'address': '0xd9ea811a51d6fe491d27c2a0442b3f577852874d',
 
+# Uniswap v3 quoter error for BOB is
+# base-memecoin-index  |   File "/usr/src/trade-executor/deps/web3-ethereum-defi/eth_defi/provider/fallback.py", line 194, in make_request
+# base-memecoin-index  |     raise ValueError(resp_data["error"])
+# base-memecoin-index  | ValueError: {'message': 'execution reverted: revert: Unexpected error', 'code': 3, 'data': '0x08c379a000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000010556e6578706563746564206572726f7200000000000000000000000000000000'}
+
 #: Historical rug pulls.
 #:
 #: These tokens have their rug pulled.
