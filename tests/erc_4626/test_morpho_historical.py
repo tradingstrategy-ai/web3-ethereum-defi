49e91bc5afb6c9584b21d07260e27dfdb3d809fd
Mikko Ohtamaa <mikko@redinnovation.com>
2025-04-09 01:28:47 +0300

    Confirm issues with Morpho vault

--- tests/erc_4626/test_morpho_historical.py
+++ tests/erc_4626/test_morpho_historical.py
@@ -78,3 +78,59 @@ def test_steakhouse_usdt(
     assert r.management_fee == 0
     assert r.performance_fee == 0
     assert r.chain == 1
+
+
+
+@pytest.mark.skip(reason="No need to implement, the vault seems to read inception APY correctly")
+def test_morpho_compounder(
+    web3: Web3,
+    tmp_path: Path,
+):
+    """Read historical data of Morpho vault.
+
+    - Caused some data corruption
+    """
+
+    token_cache = TokenDiskCache(tmp_path / "tokens.sqlite")
+    parquet_file = tmp_path / "prices.parquet"
+
+    # https://yearn.fi/vaults/1/0x0a4ea2bDe8496a878a7ca2772056a8e6fe3245c5
+    compounder = create_vault_instance(
+        web3,
+        address="0xbEef047a543E45807105E51A8BBEFCc5950fcfBa",
+        features={ERC4626Feature.morpho_like},
+        token_cache=token_cache
+    )
+
+    vaults = [
+        compounder,
+    ]
+
+    # When IPOR vault was deployed https://basescan.org/tx/0x65e66f1b8648a880ade22e316d8394ed4feddab6fc0fc5bbc3e7128e994e84bf
+    start = 19_043_398
+    end = 22_196_299
+
+    steakhouse_usdt.first_seen_at_block = start
+
+    scan_report = scan_historical_prices_to_parquet(
+        output_fname=parquet_file,
+        web3=web3,
+        web3factory=MultiProviderWeb3Factory(JSON_RPC_ETHEREUM),
+        vaults=vaults,
+        start_block=start,
+        end_block=end,
+        step=24*3600 // 12,
+        token_cache=token_cache,
+    )
+    assert scan_report["rows_written"] == 438
+
+    df = pd.read_parquet(parquet_file)
+
+    # Records are not guaranteed to be in specific order, so fix it here
+    df = df.set_index("block_number").sort_index()
+
+    r = df.iloc[-1]
+    assert r.share_price == pytest.approx(1.077792700142924944038560077)
+    assert r.management_fee == 0
+    assert r.performance_fee == 0
+    assert r.chain == 1
