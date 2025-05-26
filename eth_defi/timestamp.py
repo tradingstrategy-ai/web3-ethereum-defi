e705643800a384dfce948459645da3ab5ab9e6fd
Mikko Ohtamaa <mikko@redinnovation.com>
2025-05-26 12:57:45 +0200

    Add ERC-4626 APY estimator

--- eth_defi/timestamp.py
+++ eth_defi/timestamp.py
@@ -1,10 +1,25 @@
 """Block timestamp related utilities"""
+import dataclasses
 import datetime
+from typing import TypedDict
 
+import requests
+from eth_typing import HexStr
 from web3 import Web3
 from web3.types import BlockIdentifier
 
 from eth_defi.event_reader.conversion import convert_jsonrpc_value_to_int
+from eth_defi.utils import to_unix_timestamp
+
+
+
+@dataclasses.dataclass(frozen=True, slots=True)
+class FindBlockReply:
+    """For block number estimation by time"""
+    hash: HexStr
+    block_number: int
+    block_timestamp: datetime.datetime
+    searched_timestamp: datetime.datetime
 
 
 def get_latest_block_timestamp(web3: Web3) -> datetime.datetime:
@@ -71,3 +86,37 @@ def get_block_timestamp(web3: Web3, block_identifier: BlockIdentifier) -> dateti
         return datetime.datetime.utcfromtimestamp(ts)
     except Exception as e:
         raise RuntimeError(f"Failed to read timestamp for block {block_identifier}, chain: {web3.eth.chain_id}: {e}") from e
+
+
+def estimate_block_number_for_timestamp_by_findblock(
+    chain_id: int,
+    timestamp: datetime.datetime,
+) -> FindBlockReply:
+    """Estimate block number for a given timestamp.
+
+    - To convert timestamps to block numbers
+    - Uses `FindBlock API <https://www.findblock.xyz/>`__, using ``block/before`` API
+    - Gets the block that was finaliesd at the timestamp or before it.
+
+    :param timestamp:
+        Timestamp to estimate the block number for
+
+    :return:
+        Estimated block number
+    """
+
+    assert isinstance(timestamp, datetime.datetime), "timestamp must be a datetime object"
+    assert isinstance(chain_id, int), "chain_id must be an integer"
+
+    unix_time = to_unix_timestamp(timestamp)
+
+    response = requests.get(f"https://api.findblock.xyz/v1/chain/{chain_id}/block/before/{unix_time}?inclusive=true")
+
+    data = response.json()
+    return FindBlockReply(
+        block_number=int(data["number"]),
+        hash=HexStr(data["hash"]),
+        block_timestamp=datetime.datetime.utcfromtimestamp(data["timestamp"]),
+        searched_timestamp=timestamp,
+    )
+
---
+++ tests/erc_4626/test_4626_profitability.py
@@ -0,0 +1,88 @@
+"""Calculate ERC-4626 APY"""
+import datetime
+import os
+
+import pytest
+from eth_typing import HexAddress
+from web3 import Web3
+
+from eth_defi.erc_4626.profit_and_loss import estimate_4626_recent_profitability, estimate_4626_profitability
+from eth_defi.erc_4626.vault import ERC4626VaultInfo, ERC4626Vault
+from eth_defi.provider.multi_provider import create_multi_provider_web3
+from eth_defi.timestamp import estimate_block_number_for_timestamp_by_findblock
+from eth_defi.vault.base import VaultSpec
+
+
+JSON_RPC_BASE = os.environ.get("JSON_RPC_BASE")
+
+pytestmark = pytest.mark.skipif(JSON_RPC_BASE is None, reason="JSON_RPC_BASE needed to run these tests")
+
+
+@pytest.fixture(scope='module')
+def web3() -> Web3:
+    web3 = create_multi_provider_web3(JSON_RPC_BASE)
+    return web3
+
+
+@pytest.fixture(scope='module')
+def test_block_number() -> int:
+    return 27975506
+
+
+@pytest.fixture(scope='module')
+def ipor_usdc_address() -> HexAddress:
+    # https://app.ipor.io/fusion/base/0x45aa96f0b3188d47a1dafdbefce1db6b37f58216
+    return "0x45aa96f0b3188d47a1dafdbefce1db6b37f58216"
+
+
+@pytest.fixture(scope='module')
+def vault(web3, ipor_usdc_address) -> ERC4626VaultInfo:
+    # https://app.ipor.io/fusion/base/0x45aa96f0b3188d47a1dafdbefce1db6b37f58216
+    spec = VaultSpec(web3.eth.chain_id, ipor_usdc_address)
+    return ERC4626Vault(web3, spec)
+
+
+def test_4626_recent_profitability(
+    vault: ERC4626Vault,
+    test_block_number: int,
+):
+    """Calculate 7 days profitability for IPOR USDC Base vault.
+
+    - Use live blockchain data
+    """
+
+    profitability_data = estimate_4626_recent_profitability(
+        vault,
+        lookback_window=datetime.timedelta(days=7),
+    )
+
+    yearly_profitability = profitability_data.calculate_profitability(annualise=True)
+    assert 0 < yearly_profitability < 20
+
+    profitability = profitability_data.calculate_profitability(annualise=False)
+    assert 0 < profitability < 900
+
+
+def test_4626_profitability_historical(
+    vault: ERC4626Vault,
+):
+    """Calculate profitability for IPOR USDC Base vault.
+
+    - Use historical time range
+    """
+    web3 = vault.web3
+    chain_id = web3.eth.chain_id
+    start_at = datetime.datetime(2025, 3, 1, tzinfo=None)
+    end_at = datetime.datetime(2025, 5, 1, tzinfo=None)
+    start_block_find = estimate_block_number_for_timestamp_by_findblock(chain_id, start_at)
+    end_block_find  = estimate_block_number_for_timestamp_by_findblock(chain_id, end_at)
+
+    profitability_data = estimate_4626_profitability(
+        vault,
+        start_block=start_block_find.block_number,
+        end_block=end_block_find.block_number,
+    )
+
+    yearly_profitability = profitability_data.calculate_profitability(annualise=True)
+    assert yearly_profitability == pytest.approx(0.05319605877774247)
+
