2d8a7cab750ea155a34404e5f60ebc31e4aa8f81
Hieu Nguyen <hieu@tradingstrategy.ai>
2025-07-03 00:29:34 +0300

    Add deposit support

---
+++ eth_defi/orderly/vault.py
@@ -0,0 +1,64 @@
+from eth_typing import HexAddress
+from web3 import Web3
+from web3.contract.contract import Contract, ContractFunction
+
+from eth_defi.abi import get_deployed_contract
+
+
+class OrderlyVault:
+    def __init__(self, web3: Web3, address: str):
+        self.web3 = web3
+        self.address = address
+        self.contract = get_deployed_contract(web3, "orderly/vault.json", address)
+
+
+def deposit(
+    vault: OrderlyVault,
+    *,
+    amount: int,
+    token: Contract,
+    wallet_address: HexAddress,
+    orderly_account_id: HexAddress,
+    broker_id: str,
+    token_id: str | None = None,
+) -> tuple[ContractFunction, ContractFunction, ContractFunction]:
+    """
+    Deposit tokens into the Orderly vault.
+
+    :param vault:
+        The vault to deposit into.
+    :param amount:
+        The amount of tokens to deposit.
+    :param token:
+        The token to deposit.
+    :param wallet_address:
+        The wallet address to deposit from.
+    :param orderly_account_id:
+        The orderly account ID to deposit to.
+    :param broker_id:
+        The broker ID to deposit to.
+    :param token_id:
+        The token ID to deposit to.
+    """
+    web3 = vault.web3
+    approve_function = token.functions.approve(vault.address, amount)
+
+    # get deposit fee
+    broker_hash = web3.keccak(text=broker_id)
+    token_hash = web3.keccak(text=token_id)
+
+    deposit_input = (
+        bytes.fromhex(orderly_account_id[2:]),
+        bytes.fromhex(broker_hash.hex()[2:]),
+        bytes.fromhex(token_hash.hex()[2:]),
+        amount,
+    )
+
+    get_deposit_fee_function = vault.contract.functions.getDepositFee(
+        Web3.to_checksum_address(wallet_address),
+        deposit_input,
+    )
+
+    deposit_function = vault.contract.functions.deposit(deposit_input)
+
+    return approve_function, get_deposit_fee_function, deposit_function
