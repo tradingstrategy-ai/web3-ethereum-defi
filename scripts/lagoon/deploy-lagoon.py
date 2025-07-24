98cadf4f83ca3944d7fd25e76504ebb012cf9fd3
Mikko Ohtamaa <mikko@redinnovation.com>
2025-07-24 12:57:06 +0200

    BNB local high added

--- scripts/lagoon/deploy-lagoon.py
+++ scripts/lagoon/deploy-lagoon.py
@@ -1,54 +1,80 @@
 """An example script to deploy a Lagoon vault.
 
-- Deploy new Safe and Lagoon vault on Base
-- Allow automated trading on Uniswap v2 via TradingStrategyModuleV0
+A quick script to test/simulate Lagoon vault deployment on any chain.
+
+- Deploy a new Safe and Lagoon vault on any chain,
+  using Lagoon v0.5.0 factory contract
+- Allow automated trading on Uniswap v2 via TradingStrategyModuleV0,
+  with configurations for Uniswap, ERC-4626 vault whitelisting and such
 - Safe is 1-of-1 multisig with the deployer as the only cosigner
+
+This **cannot** be used for product deployments, only for tests,
+as it configures random Safe multisig cosigners.
 
 To run:
 
 .. code-block:: shell
 
     export PRIVATE_KEY=...
-    export JSON_RPC_BASE=...
+    export JSON_RPC_URL=$JSON_RPC_BINANCE
     SIMULATE=true python scripts/lagoon/deploy-lagoon.py
 """
 
 import logging
 import os
-import sys
+from typing import cast
 
+from web3 import Web3
+
+from eth_defi.chain import get_chain_name
+from eth_defi.erc_4626.classification import create_vault_instance, detect_vault_features
+from eth_defi.erc_4626.vault import ERC4626Vault
 from eth_defi.hotwallet import HotWallet
 from eth_defi.lagoon.deployment import LagoonDeploymentParameters, deploy_automated_lagoon_vault
 from eth_defi.provider.anvil import fork_network_anvil
 from eth_defi.provider.multi_provider import create_multi_provider_web3
-from eth_defi.token import USDC_NATIVE_TOKEN
+from eth_defi.token import USDC_NATIVE_TOKEN, USDT_NATIVE_TOKEN
 
 from eth_defi.uniswap_v2.constants import UNISWAP_V2_DEPLOYMENTS
 from eth_defi.uniswap_v2.deployment import fetch_deployment
+from eth_defi.utils import setup_console_logging
 
-logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logger = logging.getLogger(__name__)
+
 
 RANDO1 = "0xa7208b5c92d4862b3f11c0047b57a00Dc304c0f8"
 RANDO2 = "0xbD35322AA7c7842bfE36a8CF49d0F063bf83a100"
 
 
 def main():
+
+    setup_console_logging()
+
     PRIVATE_KEY = os.environ["PRIVATE_KEY"]
-    JSON_RPC_BASE = os.environ["JSON_RPC_BASE"]
+    JSON_RPC_URL = os.environ["JSON_RPC_URL"]
     SIMULATE = os.environ.get("SIMULATE")
+
+    # Comma separated list of ERC-4626 to whitelist
+    VAULTS = os.environ.get("VAULTS")
+
+    web3 = create_multi_provider_web3(JSON_RPC_URL)
+    chain_name = get_chain_name(web3.eth.chain_id)
+
+    logger.info(f"Connected to chain {chain_name}, last block is {web3.eth.block_number:,}")
 
     deployer_wallet = HotWallet.from_private_key(PRIVATE_KEY)
     asset_manager = deployer_wallet.address
+
     # Add some random multisig holders
     multisig_owners = [deployer_wallet.address, RANDO1, RANDO2]
 
     if SIMULATE:
-        print("Simulation deployment with Anvil")
-        anvil = fork_network_anvil(JSON_RPC_BASE)
+        logger.info("Simulation deployment with Anvil")
+        anvil = fork_network_anvil(JSON_RPC_URL)
         web3 = create_multi_provider_web3(anvil.json_rpc_url)
     else:
-        print("Base production deployment")
-        web3 = create_multi_provider_web3(JSON_RPC_BASE)
+        logger.info("Production deployment")
+        web3 = create_multi_provider_web3(JSON_RPC_URL)
 
     chain_id = web3.eth.chain_id
 
@@ -59,11 +85,30 @@ def main():
         init_code_hash=UNISWAP_V2_DEPLOYMENTS["base"]["init_code_hash"],
     )
 
+    if web3.eth.chain_id == 56:
+        # Binance uses USDT
+        underlying = USDT_NATIVE_TOKEN[chain_id]
+    else:
+        underlying = USDC_NATIVE_TOKEN[chain_id]
+
     parameters = LagoonDeploymentParameters(
-        underlying=USDC_NATIVE_TOKEN[chain_id],
+        underlying=underlying,
         name="Test vault",
         symbol="TEST",
     )
+
+    if VAULTS:
+        erc_4626_vault_addresses = [Web3.to_checksum_address(a.strip()) for a in VAULTS.split(",")]
+        erc_4626_vaults = []
+        for addr in erc_4626_vault_addresses:
+            logger.info("Resolving ERC-4626 vault at %s", addr)
+            features = detect_vault_features(web3, addr)
+            vault = cast(ERC4626Vault, create_vault_instance(web3, addr, features=features))
+            assert vault.is_valid(), f"Invalid ERC-4626 vault at {addr}"
+            logger.info("Preparing vault %s for whitelisting", vault.name)
+            erc_4626_vaults.append(vault)
+    else:
+        erc_4626_vaults = None
 
     deploy_info = deploy_automated_lagoon_vault(
         web3=web3,
@@ -73,11 +118,12 @@ def main():
         safe_owners=multisig_owners,
         safe_threshold=len(multisig_owners) - 1,
         uniswap_v2=uniswap_v2,
-        uniswap_v3=None,
+        uniswap_v3=True,
         any_asset=True,
+        erc_4626_vaults=erc_4626_vaults,
     )
 
-    print(f"Lagoon vault deployed:\n{deploy_info.pformat()}")
+    logger.info(f"Lagoon vault deployed:\n{deploy_info.pformat()}")
 
 
 if __name__ == "__main__":
