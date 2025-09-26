e4cbad556fd8573f82b0cbd4e8905ae9bb1e75e4
Saikat Karmakar <github.maturity983@passinbox.com>
2025-09-26 19:46:29 +0530

    feat: replaced the initial draft of the Order class with base_order.py file

--- eth_defi/gmx/order/__init__.py
+++ eth_defi/gmx/order/__init__.py
@@ -1,3 +1,3 @@
-from eth_defi.gmx.order.order import BaseOrder, OrderParams, OrderType, OrderSide
+from eth_defi.gmx.order.base_order import BaseOrder, OrderParams, OrderType, OrderSide, TransactionResult, SwapType
 
-__all__ = ["BaseOrder", "OrderParams", "OrderType", "OrderSide"]
+__all__ = ["BaseOrder", "OrderParams", "OrderType", "OrderSide", "TransactionResult", "SwapType"]
