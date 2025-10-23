e0d7d3b16441889e334fb5ec303d79dda709c0c7
Mikko Ohtamaa <mikko@redinnovation.com>
2025-10-23 16:56:14 +0200

    Add in-process cache and write lock for Euler metadata

--- eth_defi/euler/offchain_metadata.py
+++ eth_defi/euler/offchain_metadata.py
@@ -17,6 +17,7 @@ import requests
 from web3 import Web3
 from eth_typing import HexAddress
 from eth_defi.compat import native_datetime_utc_now, native_datetime_utc_fromtimestamp
+from eth_defi.utils import wait_other_writers
 
 #: Where we copy files from Euelr Github repo
 DEFAULT_CACHE_PATH = Path.home() / ".cache" / "euler"
@@ -46,6 +47,7 @@ def fetch_euler_vaults_file_for_chain(
     """Fetch and cache Euler offchain vault metadata for a given chain.
 
     - One JSON file per chain
+    - Multiprocess safe
     """
 
     assert type(chain_id) is int, "chain_id must be integer"
@@ -53,61 +55,77 @@ def fetch_euler_vaults_file_for_chain(
 
     cache_path.mkdir(parents=True, exist_ok=True)
     file = cache_path / f"euler_vaults_chain_{chain_id}.json"
+    file = file.resolve()
 
     if not now_:
         now_ = native_datetime_utc_now()
 
-    if not file.exists() or (now_ - native_datetime_utc_fromtimestamp(file.stat().st_mtime)) > max_cache_duration:
-        logger.info(f"Re-fetching cached Euler vaults file for chain {chain_id} from {github_base_url}")
-        with file.open("wt") as f:
-            url = f"{github_base_url}/{chain_id}/vaults.json"
+    # When running multiprocess vault scan, we have competition over this file write and
+    # if we do not wait the race condition may try to read zero-bytes file
+    with wait_other_writers(file):
+        if not file.exists() or (now_ - native_datetime_utc_fromtimestamp(file.stat().st_mtime)) > max_cache_duration:
+            logger.info(f"Re-fetching cached Euler vaults file for chain {chain_id} from {github_base_url}")
+            with file.open("wt") as f:
+                url = f"{github_base_url}/{chain_id}/vaults.json"
 
-            # Fetch and save the file
-            response = requests.get(url)
+                # Fetch and save the file
+                response = requests.get(url)
 
-            logger.info(f"Got response code {response.status_code} for Euler vaults file for chain {chain_id} from {url}")
+                logger.info(f"Got response code {response.status_code} for Euler vaults file for chain {chain_id} from {url}")
 
+                try:
+                    response.raise_for_status()  # Raises exception for HTTP errors
+
+                    # Check Github file looks valuew
+                    logger.info("Fetched Euler vaults file for chain %d from %s, size %d bytes", chain_id, url, len(response.text))
+                    content = json.loads(response.text)  # Validate
+                    f.write(response.text)
+
+                    logger.info(f"Wrote {file.resolve()}")
+
+                except (HTTPError, JSONDecodeError) as e:
+                    logger.warning(
+                        "Euler vault file missing for chain %d is empty, writing empty JSON object, url %s, error %s, content %s",
+                        chain_id,
+                        url,
+                        e,
+                        response.text,
+                    )
+                    f.write("{}")
+                    content = {}
+
+            # Strange Things happening here
+            assert file.stat().st_size > 0, f"File {file} is empty after writing"
+            return content
+
+        else:
+            timestamp = datetime.datetime.fromtimestamp(file.stat().st_mtime, tz=None)
+            ago = now_ - timestamp
+            logger.info(f"Using cached Euler vaults file for chain {chain_id} from {file}, last fetched at {timestamp.isoformat()}. ago {ago}")
             try:
-                response.raise_for_status()  # Raises exception for HTTP errors
-
-                # Check Github file looks valuew
-                logger.info("Fetched Euler vaults file for chain %d from %s, size %d bytes", chain_id, url, len(response.text))
-                content = json.loads(response.text)  # Validate
-                f.write(response.text)
-
-                logger.info(f"Wrote {file.resolve()}")
-
-            except (HTTPError, JSONDecodeError) as e:
-                logger.warning(
-                    "Euler vault file missing for chain %d is empty, writing empty JSON object, url %s, error %s, content %s",
-                    chain_id,
-                    url,
-                    e,
-                    response.text,
-                )
-                f.write("{}")
-                content = {}
-
-        # Strange Things happening here
-        assert file.stat().st_size > 0, f"File {file} is empty after writing"
-        return content
-
-    else:
-        timestamp = datetime.datetime.fromtimestamp(file.stat().st_mtime, tz=None)
-        ago = now_ - timestamp
-        logger.info(f"Using cached Euler vaults file for chain {chain_id} from {file}, last fetched at {timestamp.isoformat()}. ago {ago}")
-        try:
-            return json.load(open(file, "rt"))
-        except JSONDecodeError as e:
-            content = open(file, "rt").read()
-            raise RuntimeError(f"Could not parse Euler vaults file for chain {chain_id} at {file}, length {len(content)} content starts with {content[:100]!r}") from e
+                return json.load(open(file, "rt"))
+            except JSONDecodeError as e:
+                content = open(file, "rt").read()
+                raise RuntimeError(f"Could not parse Euler vaults file for chain {chain_id} at {file}, length {len(content)} content starts with {content[:100]!r}") from e
 
 
 def fetch_euler_vault_metadata(web3: Web3, vault_address: HexAddress) -> EulerVaultMetadata | None:
-    """Fetch vault metadata from offchain source."""
+    """Fetch vault metadata from offchain source.
+
+    - Do both in-process and disk cache to avoid repeated fetches
+    """
+    global _cached_vaults
+
     chain_id = web3.eth.chain_id
-    vaults = fetch_euler_vaults_file_for_chain(chain_id)
-    if vaults:
-        vault_address = web3.to_checksum_address(vault_address)
-        return vaults.get(vault_address)
-    return None
+
+    if chain_id not in _cached_vaults:
+        vaults = fetch_euler_vaults_file_for_chain(chain_id)
+        if vaults:
+            vault_address = web3.to_checksum_address(vault_address)
+            _cached_vaults[chain_id] = vaults.get(vault_address)
+
+    return _cached_vaults[chain_id]
+
+
+#: In-process cache of fetched vaults
+_cached_vaults: dict[int, dict[HexAddress, EulerVaultMetadata]] = {}
--- eth_defi/utils.py
+++ eth_defi/utils.py
@@ -2,18 +2,22 @@
 
 import calendar
 import datetime
-import logging
-import os
 import random
 import socket
 import time
 from itertools import islice
-from pathlib import Path
 from typing import Optional
 from urllib.parse import urlparse
+import logging
+import os
+from contextlib import contextmanager
+from pathlib import Path
 
+from filelock import FileLock
 import psutil
+
 from eth_typing import HexAddress, HexStr
+
 
 logger = logging.getLogger(__name__)
 
@@ -266,3 +270,72 @@ def addr(address: str | HexAddress | HexStr) -> HexAddress:
         return HexAddress(HexStr(address))
     else:
         return address
+
+
+@contextmanager
+def wait_other_writers(path: Path | str, timeout: int = 120):
+    """Wait other potential writers writing the same file.
+
+    - Work around issues when parallel unit tests and such
+      try to write the same file
+
+    Example:
+
+    .. code-block:: python
+
+        import urllib
+        import tempfile
+
+        import pytest
+        import pandas as pd
+
+
+        @pytest.fixture()
+        def my_cached_test_data_frame() -> pd.DataFrame:
+            # Al tests use a cached dataset stored in the /tmp directory
+            path = os.path.join(tempfile.gettempdir(), "my_shared_data.parquet")
+
+            with wait_other_writers(path):
+                # Read result from the previous writer
+                if not path.exists(path):
+                    # Download and write to cache
+                    urllib.request.urlretrieve("https://example.com", path)
+
+                return pd.read_parquet(path)
+
+    :param path:
+        File that is being written
+
+    :param timeout:
+        How many seconds wait to acquire the lock file.
+
+        Default 2 minutes.
+
+    :raise filelock.Timeout:
+        If the file writer is stuck with the lock.
+    """
+
+    if isinstance(path, str):
+        path = Path(path)
+
+    assert isinstance(path, Path), f"Not Path object: {path}"
+
+    assert path.is_absolute(), f"Did not get an absolute path: {path}\nPlease use absolute paths for lock files to prevent polluting the local working directory."
+
+    # If we are writing to a new temp folder, create any parent paths
+    os.makedirs(path.parent, exist_ok=True)
+
+    # https://stackoverflow.com/a/60281933/315168
+    lock_file = path.parent / (path.name + ".lock")
+
+    lock = FileLock(lock_file, timeout=timeout)
+
+    if lock.is_locked:
+        logger.info(
+            "Parquet file %s locked for writing, waiting %f seconds",
+            path,
+            timeout,
+        )
+
+    with lock:
+        yield
