9282aec3cfff5a030e4885d9fa5ca5156e54c0c0
Mikko Ohtamaa <mikko@redinnovation.com>
2025-11-07 18:13:17 +0100

    Include sBOLD

--- eth_defi/token.py
+++ eth_defi/token.py
@@ -318,7 +318,7 @@ STABLECOIN_LIKE = set(
 
 #: Stablecoins which can be used as collateral, but which also have built-in yield bearing function
 #: with rebasing.
-YIELD_BEARING_STABLES = {"sfrxUSD", "sUSDe", "sUSDai"}
+YIELD_BEARING_STABLES = {"sfrxUSD", "sUSDe", "sUSDai", "sBOLD"}
 
 #: Stablecoins plus their interest wrapped counterparts on Compound and Aave.
 #: Also contains other derivates.
