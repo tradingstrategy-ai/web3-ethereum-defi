60a47f44aeb7009e5099bde856629d031620cb41
Aviksaikat <saikickkarma@gmail.com>
2025-12-25 13:19:39 +0530

    chore: cleanup again

--- eth_defi/gmx/order/__init__.py
+++ eth_defi/gmx/order/__init__.py
@@ -25,16 +25,7 @@ from eth_defi.gmx.order.base_order import BaseOrder, OrderParams, OrderResult
 from eth_defi.gmx.order.increase_order import IncreaseOrder
 from eth_defi.gmx.order.decrease_order import DecreaseOrder
 from eth_defi.gmx.order.swap_order import SwapOrder
-from eth_defi.gmx.order.sltp_order import (
-    SLTPOrder,
-    SLTPEntry,
-    SLTPParams,
-    SLTPOrderResult,
-    DecreaseAmounts,
-    calculate_trigger_price,
-    calculate_acceptable_price,
-    get_trigger_threshold_type,
-)
+from eth_defi.gmx.order.sltp_order import SLTPOrder, SLTPEntry, SLTPParams, SLTPOrderResult, DecreaseAmounts, calculate_trigger_price, calculate_acceptable_price, get_trigger_threshold_type
 
 __all__ = [
     # Base classes
