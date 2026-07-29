5611c45cb24c1cb99ca17290b97e54db3ecb81f2
Mikko Ohtamaa <mikko@redinnovation.com>
2026-07-29 19:33:45 +0000

    Add Axis USDx stablecoins and curator

--- eth_defi/stablecoin_metadata.py
+++ eth_defi/stablecoin_metadata.py
@@ -345,6 +345,7 @@ STABLECOIN_LIKE = set(
         "USDf",
         "USDs",
         "USDt",
+        "USDx",
         "USD₮0",
         "USG",
         "USH",
@@ -411,7 +412,7 @@ STABLECOIN_LIKE = set(
 
 #: Stablecoins which can be used as collateral, but which also have built-in yield bearing function
 #: with rebasing.
-YIELD_BEARING_STABLES = {"sfrxUSD", "sUSDe", "sUSDai", "sBOLD", "sAUSD", "sUSG", "sUSN", "ynUSDx", "sNUSD", "savUSD", "syrupUSDC", "stcUSD", "apyUSD", "sUSD3"}
+YIELD_BEARING_STABLES = {"sfrxUSD", "sUSDe", "sUSDai", "sBOLD", "sAUSD", "sUSG", "sUSN", "ynUSDx", "sNUSD", "savUSD", "syrupUSDC", "stcUSD", "apyUSD", "sUSD3", "sUSDx"}
 
 #: Stablecoins plus their interest wrapped counterparts on Compound and Aave.
 #: Also contains other derivates.
