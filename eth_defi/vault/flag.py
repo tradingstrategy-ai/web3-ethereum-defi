39e5e28bd5503940efcba2742b12f2a38afe6c95
Mikko Ohtamaa <mikko@redinnovation.com>
2026-09-08 11:20:30 +0000

    notes: mark Morpho Moonwell Flagship USDC Compounder as subvault

--- eth_defi/vault/flag.py
+++ eth_defi/vault/flag.py
@@ -811,6 +811,8 @@ VAULT_FLAGS_AND_NOTES: dict[str, tuple[VaultFlag | None, str]] = {
     "0xf115c134c23c7a05fbd489a8be3116ebf54b0d9f": (VaultFlag.subvault, SUBVAULT),
     # Morpho Zircuit Finance USDC on Base Compounder
     "0x049e8aab2d3ca187e47d74cf8171ad266f18643e": (VaultFlag.subvault, SUBVAULT),
+    # Morpho Moonwell Flagship USDC Compounder (Yearn on Base)
+    "0xd5428b889621eee8060fc105aa0ab0fa2e344468": (VaultFlag.subvault, SUBVAULT),
     # Tulipa Capital USDT0
     "0xaf293898269ac7f366d0e05052b5fdfee8c8052c": (VaultFlag.irregular_reporting, IRREGULAR_REPORTING),
     # Hashfire launch fund.
